4f66ab16a1aeb554642847e5673e68e33cac202d
Michał Bieniek <michalbieniek02@gmail.com>
2024-07-23 15:15:48 +0200

    next js with ui components and authentication installed

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+  } from '@clerk/nextjs'
+  import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <>
+        <SignedOut>
+            <nav className="fixed flex px-[20%] md:px-[25%] lg:px-[30%] shadow bg-white h-16 w-[100%] justify-between items-center font-semibold text-xl">
+                <div className="inline-flex">
+                  Re<p className='text-purple-600'>cipe</p>Hub
+                </div>
+                <SignInButton/>
+            </nav>
+        </SignedOut>
+        <SignedIn>
+            <nav className="fixed flex px-[20%] md:px-[25%] lg:px-[30%] shadow bg-white h-16 w-[100%] justify-between items-center font-semibold text-xl">
+                <div className="inline-flex">
+                  <p>Re</p><p className='text-purple-600'>cipe</p><p>Hub</p> 
+                </div>
+                <Link href={'/recipes'}><Button variant='link'>Recipes</Button></Link> 
+                <Link href={'/nutrients'}><Button variant='link'>Nutrients</Button></Link> 
+                <UserButton/>
+            </nav>
+        </SignedIn>
+    </>
+)
+}
